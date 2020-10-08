ac458725e6c8dbbcc5672e124bb55d71b0426527
wangxin <wangxin@91cyt.com>
2020-10-08 20:27:15 +0800

    1

--- common/layers.py
+++ common/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from common.functions import *
-from common.util import im2col, col2im
+from .functions import *
+from .util import im2col, col2im
 
 
 class Relu:
@@ -41,7 +41,7 @@ class Affine:
     def __init__(self, W, b):
         self.W =W
         self.b = b
-        
+
         self.x = None
         self.original_x_shape = None
         # 权重和偏置参数的导数
@@ -62,7 +62,7 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
-        
+
         dx = dx.reshape(*self.original_x_shape)  # 还原输入数据的形状（对应张量）
         return dx
 
@@ -77,7 +77,7 @@ class SoftmaxWithLoss:
         self.t = t
         self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
-        
+
         return self.loss
 
     def backward(self, dout=1):
@@ -88,7 +88,7 @@ class SoftmaxWithLoss:
             dx = self.y.copy()
             dx[np.arange(batch_size), self.t] -= 1
             dx = dx / batch_size
-        
+
         return dx
 
 
@@ -119,12 +119,12 @@ class BatchNormalization:
         self.gamma = gamma
         self.beta = beta
         self.momentum = momentum
-        self.input_shape = None # Conv层的情况下为4维，全连接层的情况下为2维  
+        self.input_shape = None # Conv层的情况下为4维，全连接层的情况下为2维
 
         # 测试时使用的平均值和方差
         self.running_mean = running_mean
-        self.running_var = running_var  
-        
+        self.running_var = running_var
+
         # backward时使用的中间数据
         self.batch_size = None
         self.xc = None
@@ -139,33 +139,33 @@ class BatchNormalization:
             x = x.reshape(N, -1)
 
         out = self.__forward(x, train_flg)
-        
+
         return out.reshape(*self.input_shape)
-            
+
     def __forward(self, x, train_flg):
         if self.running_mean is None:
             N, D = x.shape
             self.running_mean = np.zeros(D)
             self.running_var = np.zeros(D)
-                        
+
         if train_flg:
             mu = x.mean(axis=0)
             xc = x - mu
             var = np.mean(xc**2, axis=0)
             std = np.sqrt(var + 10e-7)
             xn = xc / std
-            
+
             self.batch_size = x.shape[0]
             self.xc = xc
             self.xn = xn
             self.std = std
             self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
-            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var
         else:
             xc = x - self.running_mean
             xn = xc / ((np.sqrt(self.running_var + 10e-7)))
-            
-        out = self.gamma * xn + self.beta 
+
+        out = self.gamma * xn + self.beta
         return out
 
     def backward(self, dout):
@@ -188,10 +188,10 @@ class BatchNormalization:
         dxc += (2.0 / self.batch_size) * self.xc * dvar
         dmu = np.sum(dxc, axis=0)
         dx = dxc - dmu / self.batch_size
-        
+
         self.dgamma = dgamma
         self.dbeta = dbeta
-        
+
         return dx
 
 
@@ -201,12 +201,12 @@ class Convolution:
         self.b = b
         self.stride = stride
         self.pad = pad
-        
+
         # 中间数据（backward时使用）
-        self.x = None   
+        self.x = None
         self.col = None
         self.col_W = None
-        
+
         # 权重和偏置参数的梯度
         self.dW = None
         self.db = None
@@ -249,7 +249,7 @@ class Pooling:
         self.pool_w = pool_w
         self.stride = stride
         self.pad = pad
-        
+
         self.x = None
         self.arg_max = None
 
@@ -272,13 +272,13 @@ class Pooling:
 
     def backward(self, dout):
         dout = dout.transpose(0, 2, 3, 1)
-        
+
         pool_size = self.pool_h * self.pool_w
         dmax = np.zeros((dout.size, pool_size))
         dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
-        dmax = dmax.reshape(dout.shape + (pool_size,)) 
-        
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
         dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
-        
+
         return dx
