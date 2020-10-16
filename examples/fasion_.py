61052bdc1d5fd0ded9307699cf71186e042723fe
wangxin <wangxin@91cyt.com>
2020-10-16 20:49:06 +0800

    1

---
+++ examples/fasion_.py
@@ -0,0 +1,42 @@
+import keras
+from keras import layers
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
+
+class_names = [
+    'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
+    'Sneaker', 'Bag', 'Ankle boot'
+]
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+model = keras.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10)
+])
+model.compile(
+    optimizer='adam',
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=10)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
